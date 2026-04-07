4562fc93dfd834267ecfbd629c5289e86bd72642
Khodbhaya Kishan <68937629+kishan-khodbhaya@users.noreply.github.com>
2026-04-07 22:24:20 +0530

    Update gemini_api_key to empty string
    Change gemini_api_key to an empty string for security.

--- app/config.py
+++ app/config.py
@@ -2,7 +2,7 @@ from pydantic_settings import BaseSettings, SettingsConfigDict
 from functools import lru_cache
 
 class Settings(BaseSettings):
-    gemini_api_key: str = "AIzaSyCqnR3eVp7JjFFTXbsayNBGukh_KNgrzbk"
+    gemini_api_key: str = ""
     
     model_config = SettingsConfigDict(env_file=".env", env_file_encoding="utf-8", extra="ignore")
 
